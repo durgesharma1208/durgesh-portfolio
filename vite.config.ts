e6d06e76992ac0621d339c9b89e339f8c6191ef8
Durgesh SharmA <durgesharma1208@gmail.com>
2026-02-13 19:08:36 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,21 @@ import { fileURLToPath } from "url";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import { viteSingleFile } from "vite-plugin-singlefile";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), viteSingleFile()],
+  // Removed viteSingleFile() for better performance on Vercel
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
     },
   },
+  build: {
+    // Ensures nice chunking for production
+    chunkSizeWarningLimit: 1000,
+  }
 });
